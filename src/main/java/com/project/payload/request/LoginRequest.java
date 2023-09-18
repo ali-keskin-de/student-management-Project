740f3c7add58c78919928dbc4a12cb1d2dbe50f5
alkeskn85 <alkeskn85@gmail.com>
2023-09-18 22:10:59 +0200

    V4

---
+++ src/main/java/com/project/payload/request/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.project.payload.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+
+    @NotNull(message = "username must not be empty")
+    private String username;
+
+
+    @NotNull(message = "username must not be empty")
+    private String password;
+
+
+}
--- src/main/java/com/project/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/project/security/jwt/AuthTokenFilter.java
@@ -1,8 +1,10 @@
 package com.project.security.jwt;
+
 import com.project.security.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,9 +26,11 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthTokenFilter.class);
 
-    private final JwtUtils jwtUtils;
+    @Autowired
+    private  JwtUtils jwtUtils;
 
-    private final UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private  UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
